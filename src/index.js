cda70700234fdd86fbfa26ecc81d21037be06d91
JFC <chibikami@users.noreply.github.com>
2017-09-03 04:50:53 -0600

    set up redux  - added in redux  - altered the code to use the redux sample  - installed spread operator plugin for stuff

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import {render} from 'react-dom';
-import AwesomeComponent from './AwesomeComponent.jsx';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
 
-class App extends React.Component {
-    render () {
-        return (
-            <div>
-                <p> Hello FOO!</p>
-                <AwesomeComponent />
-            </div>
-        );
-    }
-}
+let store = createStore(todoApp)
 
-render(<App/>, document.getElementById('app'));
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('app')
+)
