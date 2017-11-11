a193a7a5c9d7cb22ae4a68bdf19fce97cec2e3e5
JFC <chibikami@users.noreply.github.com>
2017-11-10 23:07:49 -0700

    set up actor list
    we can now get a new state set up and also get a list of actors in a set of cards

--- src/browser/actors/ListActors.js
+++ src/browser/actors/ListActors.js
@@ -3,6 +3,10 @@ import React from 'react';
 import { compose } from 'ramda';
 import { connect } from 'react-redux';
 
+// Material UI
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import IconButton from 'material-ui/IconButton';
+import DropActor from 'material-ui/svg-icons/action/delete';
 
 const inlineStyles = {
 
@@ -18,11 +22,28 @@ class ListActors extends React.Component {
 
   render() {
     const { allActors }= this.props;
-    const items = allActors.map((a,i)=><div>
-      {a.characteristics.name}
-      -
-      {i}
-    </div>);
+    const items = allActors.map((a,i)=>
+      <Card>
+        <CardHeader
+            title={a.characteristics.name}
+            children={
+              <CardActions>
+                <IconButton tooltip="Drop Actor">
+                  <DropActor />
+                </IconButton>
+              </CardActions>
+            }
+            actAsExpander={true}
+            showExpandableButton={true}
+        />
+
+        <CardText expandable={true}>
+          Initiative: {a.combat.currentInitiative}
+          -
+          Actor Type: {a.actorSettings.ActorType}
+        </CardText>
+      </Card>
+      );
     return (
         <div>
           {items}
--- src/common/actors/actorsReducers.js
+++ src/common/actors/actorsReducers.js
@@ -8,28 +8,25 @@ export const initialState = {
 const actorsReducer = (state: ActorsState = initialState, action: Action): ActorsState => {
   switch (action.type) {
     case 'ADD_PLAYER':
-      const newActor = {
-        actorSettings:{
-          ActorType: action.payload.actorType,
-        },
-        characteristics: {
-          name: action.payload.name,
-          HitPoints: 0,
-          ResistanceCapacity: 0,
-        },
-        combat: {
-          maxActions: 0,
-          currentActions: 0,
-          currentInitiative: action.payload.initiative,
-        },
-      };
-      let allActors = state.all;
-      allActors.push(newActor);
-      return {
-        ...state,
-        all: allActors,
 
-      };
+      return Object.assign({}, state, {
+        all : state.all.concat({
+          actorSettings:{
+            ActorType: action.payload.actorType,
+          },
+          characteristics: {
+            name: action.payload.name,
+            HitPoints: 0,
+            ResistanceCapacity: 0,
+          },
+          combat: {
+            maxActions: 0,
+            currentActions: 0,
+            currentInitiative: action.payload.initiative,
+          },
+        })
+      });
+
     default:
       return state;
   }
