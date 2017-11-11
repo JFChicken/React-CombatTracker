d6b46083fd41f578b98befe35d65e53a9f604009
JFC <chibikami@users.noreply.github.com>
2017-11-11 12:45:24 -0700

    refactored combat rounds
    moved combat rounds tracker to its own class, made some minor UI changes

--- src/browser/combatRounds/CombatRoundsPage.js
+++ src/browser/combatRounds/CombatRoundsPage.js
@@ -3,143 +3,25 @@ import React from 'react';
 import { compose } from 'ramda';
 import { connect } from 'react-redux';
 
-// Redux Actions
-import { clearCombatRounds,nextCombatRound,setActions,nextAction } from '../../common/combatRounds/actions'
+// Sub Components
+import CombatRoundsTracker from './CombatRoundsTracker';
 
-// Material UI
-import RaisedButton from 'material-ui/RaisedButton';
-import IconMenu from 'material-ui/IconMenu';
-import MenuItem from 'material-ui/MenuItem';
-import IconButton from 'material-ui/IconButton';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import Paper from 'material-ui/Paper';
-import Divider from 'material-ui/Divider';
-import ArrowDropRight from 'material-ui/svg-icons/navigation-arrow-drop-right';
-
-const inlineStyles = {
-  button: {
-    margin: 12,
-  },
-  counter:{
-    minHeight: 50 ,
-    minWidth: 50,
-    margin: 5,
-    padding: '0px 5px',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent:'center',
-  },
-  CombatRoundPage:{
-    display: 'flex',
-  }
-};
-
-type CounterProps = {
-  counter: number,
-  title: string,
-};
-
-const Counter = ({counter,title}:CounterProps) => {
-
-  return (
-      <Paper
-          style={inlineStyles.counter}
-          zDepth={2}
-          children={
-            <div>
-            <span>{title} {counter}</span>
-            </div>}
-      />
-  );
-};
+const inlineStyles = {};
 
 
 class CombatRoundsPage extends React.Component {
 
-  saveSession = ()=>{
-    const { combatRound } = this.props;
-    // const { currentCombatRound, currentAction, maxAction } = combatRound;
-    // setter
-    localStorage.setItem('combatRound', JSON.stringify(combatRound));
-
-
-};
-
-  getSession = ()=>{
-    // getter
-    const newstuff = localStorage.getItem('combatRound');
-    console.log('newstuff',newstuff);
-
-};
 
   render() {
-    const { combatRound, nextCombatRound,clearCombatRounds,setActions,nextAction } = this.props;
-    const { currentCombatRound, currentAction, maxAction } = combatRound;
+
     return (
-    <Paper style={inlineStyles.CombatRoundPage} zDepth={1}  children={
-        <div style={inlineStyles.CombatRoundPage}>
-          <Counter counter={currentCombatRound} title="Round"/>
-          <Counter counter={currentAction} title="Action"/>
-          <Counter counter={maxAction} title="Max Actions"/>
-          <RaisedButton
-              label="Next Action"
-              primary={true}
-              style={inlineStyles.button}
-              onClick={()=>{nextAction()}}
-          />
-          <IconMenu
-              iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-              anchorOrigin={{horizontal: 'left', vertical: 'top'}}
-              targetOrigin={{horizontal: 'left', vertical: 'top'}}
-          >
-            <MenuItem
-                primaryText="Set Actions"
-                rightIcon={<ArrowDropRight />}
-                menuItems={[
-                  <MenuItem primaryText="4"
-                            onClick={()=>{setActions(4)}}/>,
-                  <MenuItem primaryText="5"
-                            onClick={()=>{setActions(5)}}/>,
-                  <MenuItem primaryText="6"
-                            onClick={()=>{setActions(6)}}  />,
-                ]}
-            />
-          <Divider/>
-            <MenuItem
-                primaryText="Save Combat"
-                onClick={()=>{this.saveSession()}}
-            />
-            <MenuItem
-                primaryText="Get Combat"
-                onClick={()=>{this.getSession()}}
-            />
-
-            <Divider/>
-
-            <MenuItem
-                primaryText="Next Round"
-                onClick={()=>{nextCombatRound()}}
-            />
-            <MenuItem
-                primaryText="Clear Combat"
-                onClick={()=>{clearCombatRounds()}}
-            />
-
-          </IconMenu>
-        </div>
-    } />
+        <CombatRoundsTracker/>
     );
   }
 }
 export default compose(
     connect(
-        (state) => ({
-          combatRound: state.combatRound,
-        }),
-        { nextCombatRound,
-          clearCombatRounds,
-          setActions,
-          nextAction,
-        },
+        ( state ) => ({}),
+        {},
     ),
 )(CombatRoundsPage);
